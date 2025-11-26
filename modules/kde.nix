394dd08a04023ad1d27730b1c75aba6ce3984928
g0r3 <3685646+g0r3@users.noreply.github.com>
2025-11-26 09:05:15 +0100

    add lock screen config to kde settings

--- modules/kde.nix
+++ modules/kde.nix
@@ -28,6 +28,7 @@
     shutdown-or-switch
     window-title-applet
     kdePackages.merkuro
+    kdePackages.sddm-kcm
     kdePackages.kscreenlocker
     kdePackages.kcalc
     kdePackages.kdeconnect-kde

2f931557f29accf879315407eb9cb4d7c99403c0
g0r3 <3685646+g0r3@users.noreply.github.com>
2026-01-06 12:40:48 +0100

    Copy app packages instead of linking them

--- machines/macbook/configuration.nix
+++ machines/macbook/configuration.nix
@@ -181,7 +181,7 @@
       while read -r src; do
         app_name=$(basename "$src")
         echo "copying $src" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        cp -R "$src" "/Applications/Nix Apps/$app_name"
       done
     '';
 }

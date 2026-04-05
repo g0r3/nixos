c0618f03b4a8c76e27e464c25f343b3d5f65a345
g0r3 <3685646+g0r3@users.noreply.github.com>
2026-04-05 17:58:01 +0200

    Fix steam module for macbook

--- modules/steam.nix
+++ modules/steam.nix
@@ -3,6 +3,7 @@
   lib,
   pkgs,
   isLinux,
+  isDarwin,
   ...
 }:
 let
@@ -11,33 +12,48 @@ in
 {
   options.modules.steam.enable = lib.mkEnableOption "Whether to enable the Steam module";
 
-  config = lib.optionalAttrs isLinux (lib.mkIf cfg.enable {
-    programs = {
-      gamemode.enable = true;
-      gamescope = {
-        enable = true;
-        capSysNice = true;
-      };
-      steam = {
-        enable = true;
-        gamescopeSession.enable = true;
-      };
-    };
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      (lib.optionalAttrs isLinux {
+        programs = {
+          gamemode.enable = true;
+          gamescope = {
+            enable = true;
+            capSysNice = true;
+          };
+          steam = {
+            enable = true;
+            gamescopeSession.enable = true;
+          };
+        };
 
-    services.displayManager.gdm.wayland = true;
-    systemd.user.services.steam = {
-      description = "Start Steam after graphical login";
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      serviceConfig = {
-        Environment = [
-          "PATH=/run/current-system/sw/bin"
-        ];
-        ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-        ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
-        Restart = "on-failure";
-        RestartSec = "5s";
-      };
-    };
-  });
+        services.displayManager.gdm.wayland = true;
+        systemd.user.services.steam = {
+          description = "Start Steam after graphical login";
+          wantedBy = [ "graphical-session.target" ];
+          partOf = [ "graphical-session.target" ];
+          serviceConfig = {
+            Environment = [
+              "PATH=/run/current-system/sw/bin"
+            ];
+            ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+            ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
+            Restart = "on-failure";
+            RestartSec = "5s";
+          };
+        };
+      })
+      (lib.optionalAttrs isDarwin {
+        homebrew = {
+          enable = true;
+          casks = [
+            "steam" # not yet supported on nix-darwin as of now
+          ];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
+      })
+    ]
+  );
 }
